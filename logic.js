770a110d68670a1e851aa3f600d03d75db28b26f
Marcos Henzel <marcoshenzel@gmail.com>
2020-07-19 20:06:38 -0300

    Update logic.js
    Alterando a origem da música padrão do alarme;

--- logic.js
+++ logic.js
@@ -10,7 +10,7 @@ function init() {
 	var elemento_audio = document.createElement('audio');//VARIÁVEL PARA CRIAR O ELEMENTO AUDIO
 	var urlMp3 = document.createElement('p');//VARIÁVEL PARA CRIAR UM ELEMENTO P
 	const button = document.querySelector('button') //CONSTANTE PARA MANIPULAR EVENTOS NO BOTÃO
-	var srcPadrao = 'https://www.botecodigital.info/exemplos/audio/i_am_the_doctor.mp3';
+	var srcPadrao = '/diversos/sounds/i_am_the_doctor.mp3';
 	//FINAL DA DECLARAÇÃO DAS VARIÁVEIS
 
 	//INÍCIO AUDIO DIV
